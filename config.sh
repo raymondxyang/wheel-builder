2f1d37914883aeda8dfede4014091dba55efe23b
Raymond Yang <zihao.yang@microsoft.com>
2018-06-14 14:15:14 -0700

    Try debug wheel with debug symbols

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 function build_wheel {
     build_libs
     export ONNX_ML=1
-    export CMAKE_BUILD_TYPE=Debug
+    export CMAKE_BUILD_TYPE=RelWithDebInfo
     cd $REPO_DIR
     local current_dir="$(pwd)"
     echo Current at ${current_dir}
