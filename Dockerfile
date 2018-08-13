2f221bb9147e3b5a427a6e7b33f736311cdb0368
Raymond Yang <zihao.yang@microsoft.com>
2018-08-13 16:25:32 -0700

    Try update to use Pytorch's build Pipeline

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+FROM ubuntu:16.04
+
+ENV LC_ALL en_US.UTF-8
+ENV LANG en_US.UTF-8
+ENV LANGUAGE en_US.UTF-8
+
+RUN yum install -y wget curl perl util-linux xz bzip2 git patch which perl
+RUN yum install -y yum-utils centos-release-scl
+RUN yum-config-manager --enable rhel-server-rhscl-7-rpms
+RUN yum install -y devtoolset-3-gcc devtoolset-3-gcc-c++ devtoolset-3-gcc-gfortran devtoolset-3-binutils
+ENV PATH=/opt/rh/devtoolset-3/root/usr/bin:$PATH
+ENV LD_LIBRARY_PATH=/opt/rh/devtoolset-3/root/usr/lib64:/opt/rh/devtoolset-3/root/usr/lib:$LD_LIBRARY_PATH
+
+# EPEL for cmake
+RUN wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm && \
+    rpm -ivh epel-release-latest-7.noarch.rpm && \
+    rm -f epel-release-latest-7.noarch.rpm
+
+# Build protobuf
+RUN mkdir -p /usr/local/protobuf && \
+    wget -qO- "https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz" | tar -xz -C /usr/local/protobuf --strip-components 1 && \
+    cd /usr/local/protobuf && ./configure && make check && make install && ldconfig
+
+
+# cmake
+RUN yum install -y cmake3 && \
+    ln -s /usr/bin/cmake3 /usr/bin/cmake
+
+# build python
+COPY build_scripts /build_scripts
+RUN bash build_scripts/build.sh && rm -r build_scripts
+
+ENV SSL_CERT_FILE=/opt/_internal/certs.pem
+
+RUN rm -f /usr/local/bin/patchelf
+RUN git clone https://github.com/NixOS/patchelf && \
+    cd patchelf && \
+    sed -i 's/serial/parallel/g' configure.ac && \
+    ./bootstrap.sh && \
+    ./configure && \
+    make && \
+    make install && \
+    cd .. && \
+    rm -rf patchelf
---
+++ build.sh
@@ -0,0 +1,209 @@
+#!/usr/bin/env bash
+
+set -ex
+
+export ONNX_ML=1
+WHEELHOUSE_DIR="wheelhouse"
+
+# Build for given Python versions, or for all in /opt/python if none given
+if [[ -z "$DESIRED_PYTHON" ]]; then
+  DESIRED_PYTHON=($(ls -d /opt/python/*/))
+fi
+for (( i=0; i<"${#DESIRED_PYTHON[@]}"; i++ )); do
+  # Convert eg. cp27-cp27m to /opt/python/cp27-cp27m
+  if [[ ! -d "${DESIRED_PYTHON[$i]}" ]]; then
+    if [[ -d "/opt/python/${DESIRED_PYTHON[$i]}" ]]; then
+      DESIRED_PYTHON[$i]="/opt/python/${DESIRED_PYTHON[$i]}"
+    else
+      echo "Error: Given Python ${DESIRED_PYTHON[$i]} is not in /opt/python"
+      echo "All array elements of env variable DESIRED_PYTHON must be"
+      echo "valid Python installations under /opt/python"
+      exit 1
+    fi
+  fi
+done
+echo "Will build for all Pythons: ${DESIRED_PYTHON[@]}"
+
+ONNX_DIR="/onnx"
+if [[ ! -d "ONNX_DIR" ]]; then
+  git clone https://github.com/onnx/onnx $ONNX_DIR
+  pushd $ONNX_DIR
+  if ! git checkout v${ONNX_BUILD_VERSION}; then
+      git checkout tags/v${ONNX_BUILD_VERSION}
+  fi
+else
+  # the pytorch dir will already be cloned and checked-out on jenkins jobs
+  pushd $ONNX_DIR
+fi
+git submodule update --init --recursive
+
+OLD_PATH=$PATH
+for PYDIR in "${DESIRED_PYTHON[@]}"; do
+    export PATH=$PYDIR/bin:$OLD_PATH
+    python setup.py clean
+    if [[ $PYDIR  == "/opt/python/cp37-cp37m" ]]; then
+	pip install numpy==1.15
+    else
+	pip install numpy==1.11
+    fi
+    time python setup.py bdist_wheel -d $WHEELHOUSE_DIR
+done
+
+popd
+
+#######################################################################
+# ADD DEPENDENCIES INTO THE WHEEL
+#
+# auditwheel repair doesn't work correctly and is buggy
+# so manually do the work of copying dependency libs and patchelfing
+# and fixing RECORDS entries correctly
+######################################################################
+yum install -y zip openssl
+
+fname_with_sha256() {
+    HASH=$(sha256sum $1 | cut -c1-8)
+    DIRNAME=$(dirname $1)
+    BASENAME=$(basename $1)
+	INITNAME=$(echo $BASENAME | cut -f1 -d".")
+	ENDNAME=$(echo $BASENAME | cut -f 2- -d".")
+	echo "$DIRNAME/$INITNAME-$HASH.$ENDNAME"
+}
+
+make_wheel_record() {
+    FPATH=$1
+    if echo $FPATH | grep RECORD >/dev/null 2>&1; then
+	# if the RECORD file, then
+	echo "$FPATH,,"
+    else
+	HASH=$(openssl dgst -sha256 -binary $FPATH | openssl base64 | sed -e 's/+/-/g' | sed -e 's/\//_/g' | sed -e 's/=//g')
+	FSIZE=$(ls -nl $FPATH | awk '{print $5}')
+	echo "$FPATH,sha256=$HASH,$FSIZE"
+    fi
+}
+
+
+mkdir -p /$WHEELHOUSE_DIR
+cp $ONNX_DIR/$WHEELHOUSE_DIR/*.whl /$WHEELHOUSE_DIR
+mkdir /tmp_dir
+pushd /tmp_dir
+
+DEPS_LIST=(
+    "/usr/local/protobuf/lib64/libprotobuf.so.9.0.1"
+)
+DEPS_SONAME=(
+    "libprotobuf.so.9.0.1"
+)
+
+
+for whl in /$WHEELHOUSE_DIR/onnx*linux*.whl; do
+    rm -rf tmp
+    mkdir -p tmp
+    cd tmp
+    cp $whl .
+
+    unzip -q $(basename $whl)
+    rm -f $(basename $whl)
+
+    # copy over needed dependent .so files over and tag them with their hash
+    patched=()
+    for filepath in "${DEPS_LIST[@]}"
+    do
+	filename=$(basename $filepath)
+	destpath=onnx/.lib/$filename
+	if [[ "$filepath" != "$destpath" ]]; then
+	    cp $filepath $destpath
+	fi
+
+	patchedpath=$(fname_with_sha256 $destpath)
+	patchedname=$(basename $patchedpath)
+	if [[ "$destpath" != "$patchedpath" ]]; then
+	    mv $destpath $patchedpath
+	fi
+	patched+=("$patchedname")
+	echo "Copied $filepath to $patchedpath"
+    done
+
+    echo "patching to fix the so names to the hashed names"
+    for ((i=0;i<${#DEPS_LIST[@]};++i));
+    do
+	find onnx -name '*.so*' | while read sofile; do
+	    origname=${DEPS_SONAME[i]}
+	    patchedname=${patched[i]}
+	    if [[ "$origname" != "$patchedname" ]]; then
+		set +e
+		patchelf --print-needed $sofile | grep $origname 2>&1 >/dev/null
+		ERRCODE=$?
+		set -e
+		if [ "$ERRCODE" -eq "0" ]; then
+		    echo "patching $sofile entry $origname to $patchedname"
+		    patchelf --replace-needed $origname $patchedname $sofile
+		fi
+	    fi
+	done
+    done
+
+    # set RPATH of _C.so and similar to $ORIGIN, $ORIGIN/lib
+    find onnx -maxdepth 1 -type f -name "*.so*" | while read sofile; do
+	echo "Setting rpath of $sofile to " '$ORIGIN:$ORIGIN/.lib'
+	patchelf --set-rpath '$ORIGIN:$ORIGIN/.lib' $sofile
+	patchelf --print-rpath $sofile
+    done
+
+    # set RPATH of lib/ files to $ORIGIN
+    find onnx/.lib -maxdepth 1 -type f -name "*.so*" | while read sofile; do
+	echo "Setting rpath of $sofile to " '$ORIGIN'
+	patchelf --set-rpath '$ORIGIN' $sofile
+	patchelf --print-rpath $sofile
+    done
+
+
+    # regenerate the RECORD file with new hashes
+    record_file=`echo $(basename $whl) | sed -e 's/-cp.*$/.dist-info\/RECORD/g'`
+    echo "Generating new record file $record_file"
+    rm -f $record_file
+    # generate records for torch folder
+    find onnx -type f | while read fname; do
+	echo $(make_wheel_record $fname) >>$record_file
+    done
+    # generate records for torch-[version]-dist-info folder
+    find onnx*dist-info -type f | while read fname; do
+	echo $(make_wheel_record $fname) >>$record_file
+    done
+
+    # zip up the wheel back
+    zip -rq $(basename $whl) onnx*
+
+    # replace original wheel
+    rm -f $whl
+    mv $(basename $whl) $whl
+    cd ..
+    rm -rf tmp
+done
+
+# Print out sizes of all wheels created
+echo "Succesfulle made wheels of size:"
+du -h /$WHEELHOUSE_DIR/onnx*.whl
+
+# Copy wheels to host machine for persistence after the docker
+mkdir -p /remote/$WHEELHOUSE_DIR
+cp /$WHEELHOUSE_DIR/onnx*.whl /remote/$WHEELHOUSE_DIR/
+
+
+# The package's name that we made could be torch-nightly
+if [[ -n "$ONNX_PACKAGE_NAME" ]]; then
+  package_name="$ONNX_PACKAGE_NAME"
+else
+  package_name='onnx'
+fi
+echo "Expecting the built wheels to be packages for '$package_name'"
+
+
+# Test that all the wheels work
+for PYDIR in "${DESIRED_PYTHON[@]}"; do
+    "${PYDIR}/bin/pip" uninstall -y "$package_name"
+    "${PYDIR}/bin/pip" install tornado==4.5.3 pytest-cov nbval
+    "${PYDIR}/bin/pip" install "$package_name" --no-index -f /$WHEELHOUSE_DIR
+    set +e
+    cd ONNX_DIR && pytest
+    set -e
+done
